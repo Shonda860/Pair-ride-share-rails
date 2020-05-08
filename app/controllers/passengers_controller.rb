cc58369ee31c1d96747ab4d26d206d56a5ec3fec
freeMonkey19 <freeMonkey19@gmail.com>
2020-05-08 13:24:13 -0700

    create show method in controller and render view

--- app/controllers/passengers_controller.rb
+++ app/controllers/passengers_controller.rb
@@ -4,17 +4,28 @@ class PassengersController < ApplicationController
 			@passengers = Passenger.all
     end
 
-    def show
-    end
+		def show # get one passenger
+			# id passed into the route
+			id = params[:id]
+			@passenger =  Passenger.find_by(id:id)
+	
+			if @passenger.nil?
+				redirect_to trips_path
+				return
+			end
+		end
+  
 
     def edit
     end
 
-		def new #render the form for create
+		def new # renders form for create method
+			#creates local object but does not validate or save to db
 			@passenger = Passenger.new
     end
 
-    def create
+		def create 
+			@passenger = Passenger.new(params[:passenger]) #instantiate new passenger w/strong params
     end
 
     def update
@@ -25,7 +36,7 @@ class PassengersController < ApplicationController
 		
 		private
 
-		def passenger_params
+		def passenger_params #passenger_id needs to be added here? it's nil in db
 			return params.require(:passenger).permit(:name, :phone_num)
 		end
 

f7f9109f20a73d6f498c44ba7b3e9fdd0b4e8650
freeMonkey19 <freeMonkey19@gmail.com>
2020-05-08 11:22:29 -0700

    create Passenger index method/properly display list in browser

--- app/controllers/passengers_controller.rb
+++ app/controllers/passengers_controller.rb
@@ -1,2 +1,32 @@
 class PassengersController < ApplicationController
+
+		def index #list all 
+			@passengers = Passenger.all
+    end
+
+    def show
+    end
+
+    def edit
+    end
+
+		def new #render the form for create
+			@passenger = Passenger.new
+    end
+
+    def create
+    end
+
+    def update
+    end
+
+    def destroy
+		end
+		
+		private
+
+		def passenger_params
+			return params.require(:passenger).permit(:name, :phone_num)
+		end
+
 end

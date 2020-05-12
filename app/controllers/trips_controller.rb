17a0faa71d2b1df9ac670f75bf84452d41c0edff
Shonda860 <norshonda.warren@my.ccsu.edu>
2020-05-12 00:32:25 -0700

    comment for index-trips

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -1,5 +1,5 @@
 class TripsController < ApplicationController
-  def show # may need a redirect
+  def show # may need a redirect for index
     @trip = Trip.find_by(id: params[:id].to_i)
     if @trip.nil?
       redirect_to trips_path

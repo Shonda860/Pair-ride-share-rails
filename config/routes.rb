4489a0e1a74247bc7fb7ff1b9c22b012d2039ec3
Shonda860 <norshonda.warren@my.ccsu.edu>
2020-05-12 18:46:21 -0700

    final updates

--- config/routes.rb
+++ config/routes.rb
@@ -4,47 +4,58 @@ Rails.application.routes.draw do
 
   root to: "homepages#index"
 
+  resources :drivers do
+    resources :trips, only: [:show]
+  end
+  resources :passengers do
+    resources :trips
+  end
 
-  # TO DO:
-  # refactor routes using resources
-  # remove all routes except custom routes
-  # something like this:
-  # resources :passengers
-  # resources :drivers
-  # resources :trips
+  resources :trips, only: [:show, :destroy, :edit, :delete]
 
-  # TO DO:
-  # create a root_path
-  # root to: "something#index"
-
-  # get "/trips", to: "trips#index", as: :trips # all trips #EXCLUDES
-  get "/trips/new", to: "trips#new", as: :new_trip
-  post "/trips", to: "trips#create"
-
-  get "/trips/:id", to: "trips#show", as: :trip
-  get "/trips/:id/edit", to: "trips#edit", as: :edit_trip
-  patch "/trips/:id", to: "trips#update"
-  put "/trips/:id", to: "trips#update"
-  delete "/trips/:id", to: "trips#destroy"
-
-  get "/drivers", to: "drivers#index", as: :drivers # all drivers
-  get "/drivers/new", to: "drivers#new", as: :new_driver
-  post "/drivers", to: "drivers#create"
-
-  get "/drivers/:id", to: "drivers#show", as: :driver
-  get "/drivers/:id/edit", to: "drivers#edit", as: :edit_driver
-  delete "/drivers/:id", to: "drivers#destroy"
-  patch "/drivers/:id", to: "drivers#update"
-
-  get "/passengers", to: "passengers#index", as: :passengers # all passengers
-  post "/passengers", to: "passengers#create"
-  get "/passengers/new", to: "passengers#new", as: :new_passenger
-  get "/passengers/:id", to: "passengers#show", as: :passenger
-  patch "/passengers/:id", to: "passengers#update"
-  put "/passengers/:id", to: "passengers#update", as: :update_passenger
-  delete "/passengers/:id", to: "passengers#destroy", as: :delete_passenger
-  get "/passengers/:id/edit", to: "passengers#edit", as: :edit_passenger
-  patch "/passengers/:id", to: "passengers#update"
+  post "/passengers/:id/trips ", to: "trips#passenger_trips"
 end
+
+# TO DO:
+# refactor routes using resources
+# remove all routes except custom routes
+# something like this:
+# resources :passengers
+# resources :drivers
+# resources :trips
+
+# TO DO:
+# create a root_path
+# root to: "something#index"
+
+# get "/trips", to: "trips#index", as: :trips # all trips #EXCLUDES
+# get "/trips/new", to: "trips#new", as: :new_trip
+# post "/trips", to: "trips#create"
+
+# get "/trips/:id", to: "trips#show", as: :trip
+# get "/trips/:id/edit", to: "trips#edit", as: :edit_trip
+# patch "/trips/:id", to: "trips#update"
+# put "/trips/:id", to: "trips#update"
+# delete "/trips/:id", to: "trips#destroy"
+
+# get "/drivers", to: "drivers#index", as: :drivers # all drivers
+# get "/drivers/new", to: "drivers#new", as: :new_driver
+# post "/drivers", to: "drivers#create"
+
+# get "/drivers/:id", to: "drivers#show", as: :driver
+# get "/drivers/:id/edit", to: "drivers#edit", as: :edit_driver
+# delete "/drivers/:id", to: "drivers#destroy"
+# patch "/drivers/:id", to: "drivers#update"
+
+# get "/passengers", to: "passengers#index", as: :passengers # all passengers
+# post "/passengers", to: "passengers#create"
+# get "/passengers/new", to: "passengers#new", as: :new_passenger
+# get "/passengers/:id", to: "passengers#show", as: :passenger
+# patch "/passengers/:id", to: "passengers#update"
+# put "/passengers/:id", to: "passengers#update", as: :update_passenger
+# delete "/passengers/:id", to: "passengers#destroy", as: :delete_passenger
+# get "/passengers/:id/edit", to: "passengers#edit", as: :edit_passenger
+# patch "/passengers/:id", to: "passengers#update"
+# end
 
 # This comment is for testing purposes only
